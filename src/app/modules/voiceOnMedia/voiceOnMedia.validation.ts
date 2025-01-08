f76672aa9f1aac47c552888bc8f7eaf50573b706
MD. SHIPON <sshimi910@gmail.com>
2025-01-07 17:05:32 -0800

    voiceOnMedia validation updated

--- src/app/modules/voiceOnMedia/voiceOnMedia.validation.ts
+++ src/app/modules/voiceOnMedia/voiceOnMedia.validation.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 const createVoiceOnMediaValidationSchema = z.object({
   body: z.object({
     title: z.string({ invalid_type_error: 'title must be string' }),
-    date: z.string({ invalid_type_error: 'date must be string' }),
-    // videoUrl: z.string({ invalid_type_error: 'videoUrl must be string' }),
+    // date: z.string({ invalid_type_error: 'date must be string' }),
+    videoUrl: z.string({ invalid_type_error: 'videoUrl must be string' }),
   }),
 });
 
